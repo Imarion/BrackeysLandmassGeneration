ce278c840b5eda0f19936853e4e2308f4f62f019
emr <renquin@awtce.be>
2019-02-22 13:57:52 +0100

    Episode 4: add regions.

--- Assets/Scripts/MapDisplay.cs
+++ Assets/Scripts/MapDisplay.cs
@@ -18,26 +18,10 @@ public class MapDisplay : MonoBehaviour
         
     }
 
-    public void DrawNoiseMap(float[,] noiseMap) {
-        int width = noiseMap.GetLength(0);
-        int height = noiseMap.GetLength(1);
+    public void DrawTexture(Texture2D texture) {
 
-        Texture2D texture = new Texture2D(width, height);
-
-        // faster to create an array and set all the pixels at once than loop through and set all pixels individually
-        Color[] colourMap = new Color[width * height];
-        for (int y = 0; y < height; y++)
-        {
-            for (int x = 0; x < width; x++)
-            {
-                colourMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
-            }
-        }
-
-        texture.SetPixels(colourMap);
-        texture.Apply();
 
         textureRender.sharedMaterial.mainTexture = texture;
-        textureRender.transform.localScale = new Vector3(width, 1, height);
+        textureRender.transform.localScale = new Vector3(texture.width, 1, texture.height);
     }
 }
--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class MapGenerator : MonoBehaviour
 {
+    public enum DrawMode { NoiseMap, ColourMap };
+    public DrawMode drawMode;
+
     public int mapWidth, mapHeight;
     public float noiseScale;
 
@@ -16,6 +19,8 @@ public class MapGenerator : MonoBehaviour
     public Vector2 offset;
 
     public bool autoUpdate;
+
+    public TerrainType[] regions;
 
     // Start is called before the first frame update
     void Start()
@@ -32,8 +37,32 @@ public class MapGenerator : MonoBehaviour
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
 
+        Color[] colourMap = new Color[mapWidth * mapHeight];
+        for (int y = 0; y < mapHeight; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeight = noiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeight <= regions[i].height) {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+
+        }
+
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap)
+        {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        }
+        else {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
+        
     }
 
     private void OnValidate()
@@ -56,4 +85,11 @@ public class MapGenerator : MonoBehaviour
             octaves = 0;
         }        
     }
+}
+
+[System.Serializable]
+public struct TerrainType {
+    public string name;
+    public float height;
+    public Color colour;
 }
