6b77700190e98768fa06e6a5eaf4e916b875d757
emr <renquin@awtce.be>
2019-03-01 13:23:44 +0100

    Episode 10: seams

--- Assets/Scripts/EndlessTerrain.cs
+++ Assets/Scripts/EndlessTerrain.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class EndlessTerrain : MonoBehaviour
 {
+    const float scale = 1f;
+
     const float viewerMoveThresholdForChunkUpdate = 25f;  // distance the viewer has to move before we start updating all the chunks
     const float sqrViewerMoveThresholdForChunkUpdate = viewerMoveThresholdForChunkUpdate * viewerMoveThresholdForChunkUpdate;
 
@@ -20,7 +22,7 @@ public class EndlessTerrain : MonoBehaviour
     int chunksVisibleInViewDst;
 
     Dictionary<Vector2, TerrainChunk> terrainchunkDictionary = new Dictionary<Vector2, TerrainChunk>();
-    List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
+    static List<TerrainChunk> terrainChunksVisibleLastUpdate = new List<TerrainChunk>();
 
     // Start is called before the first frame update
     void Start()
@@ -35,7 +37,7 @@ public class EndlessTerrain : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        viewerPosition = new Vector2(viewer.position.x, viewer.position.z);
+        viewerPosition = new Vector2(viewer.position.x, viewer.position.z) / scale;
 
         if ((viewerPositionOld - viewerPosition).sqrMagnitude > sqrViewerMoveThresholdForChunkUpdate) {
             viewerPositionOld = viewerPosition;
@@ -63,9 +65,6 @@ public class EndlessTerrain : MonoBehaviour
 
                 if (terrainchunkDictionary.ContainsKey(viewedChunkCoord)) {
                     terrainchunkDictionary[viewedChunkCoord].UpdateTerrainChunk();
-                    if (terrainchunkDictionary[viewedChunkCoord].IsVisible()) {
-                        terrainChunksVisibleLastUpdate.Add(terrainchunkDictionary[viewedChunkCoord]);
-                    }
                 }
                 else {
                     terrainchunkDictionary.Add(viewedChunkCoord, new TerrainChunk(viewedChunkCoord, chunkSize, detailLevels, transform, mapMaterial));
@@ -101,8 +100,9 @@ public class EndlessTerrain : MonoBehaviour
             meshFilter = meshObject.AddComponent<MeshFilter>();
             meshRenderer.material = material;
 
-            meshObject.transform.position = positionV3;
+            meshObject.transform.position = positionV3 * scale;
             meshObject.transform.parent = parent;
+            meshObject.transform.localScale = Vector3.one * scale;
             SetVisible(false);
 
             lodMeshes = new LODMesh[detailsLevels.Length];
@@ -164,6 +164,9 @@ public class EndlessTerrain : MonoBehaviour
                             lodMesh.RequestMesh(mapData);
                         }
                     }
+
+                    terrainChunksVisibleLastUpdate.Add(this);
+
                 }
 
                 SetVisible(visible);
--- Assets/Scripts/Noise.cs
+++ Assets/Scripts/Noise.cs
@@ -4,25 +4,34 @@ using UnityEngine;
 
 public static class Noise
 {
-    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset) {
+    public enum NormalizeMode { Local, Global };
+
+    public static float[,] GenerateNoiseMap(int mapWidth, int mapHeight, int seed, float scale, int octaves, float persistance, float lacunarity, Vector2 offset, NormalizeMode normalizeMode) {
         float[,] noiseMap = new float[mapWidth, mapHeight];
 
         System.Random prng = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
 
+        float maxPossibleHeight = 0f;
+        float amplitude = 1f;
+        float frequency = 1f;
+
         for (int i = 0; i < octaves; i++)
         {
             float offsetX = prng.Next(-100000, 100000) + offset.x;
-            float offsetY = prng.Next(-100000, 100000) + offset.y;
+            float offsetY = prng.Next(-100000, 100000) - offset.y;
             octaveOffsets[i] = new Vector2(offsetX, offsetY);
+
+            maxPossibleHeight += amplitude;
+            amplitude *= persistance;
         }
 
         if (scale <= 0f) {
             scale = 0.0001f;
         }
 
-        float maxNoiseHeight = float.MinValue;
-        float minNoiseHeight = float.MaxValue;
+        float maxLocalNoiseHeight = float.MinValue;
+        float minLocalNoiseHeight = float.MaxValue;
 
         // used to zoom in in the cente of the map instead of the top right corner
         float halfWidth = mapWidth / 2f;
@@ -32,14 +41,14 @@ public static class Noise
         {
             for (int x = 0; x < mapWidth; x++)
             {
-                float amplitude = 1f;
-                float frequency = 1f;
+                amplitude = 1f;
+                frequency = 1f;
                 float noiseHeight = 0f;
 
                 for (int i = 0; i < octaves; i++)
                 {
-                    float samplex = (x - halfWidth) / scale * frequency + octaveOffsets[i].x;
-                    float sampley = (y - halfHeight) / scale * frequency + octaveOffsets[i].y;
+                    float samplex = (x - halfWidth + octaveOffsets[i].x) / scale * frequency;
+                    float sampley = (y - halfHeight + octaveOffsets[i].y) / scale * frequency;
 
                     float perlinValue = Mathf.PerlinNoise(samplex, sampley) * 2 - 1; // range of Perlinoise [0 1] -> *2-1 gives range [-1 +1]
                     noiseHeight += perlinValue * amplitude;
@@ -48,12 +57,12 @@ public static class Noise
                     frequency *= lacunarity;                    
                 }
 
-                if (noiseHeight > maxNoiseHeight)
+                if (noiseHeight > maxLocalNoiseHeight)
                 {
-                    maxNoiseHeight = noiseHeight;
+                    maxLocalNoiseHeight = noiseHeight;
                 }
-                else {
-                    minNoiseHeight = noiseHeight;
+                else if (noiseHeight < minLocalNoiseHeight) {
+                    minLocalNoiseHeight = noiseHeight;
                 }
 
                 noiseMap[x, y] = noiseHeight;
@@ -65,7 +74,16 @@ public static class Noise
             for (int x = 0; x < mapWidth; x++)
             {
                 // Normalize noiseMap
-                noiseMap[x, y] = Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, noiseMap[x, y]); // InverseLerp returns value between 0 and 1
+                if (normalizeMode == NormalizeMode.Local)
+                {
+                    noiseMap[x, y] = Mathf.InverseLerp(minLocalNoiseHeight, maxLocalNoiseHeight, noiseMap[x, y]); // InverseLerp returns value between 0 and 1
+                }
+                else
+                {
+                    //float normalizeHeight = (noiseMap[x, y] + 1) / (2f * maxPossibleHeight / 1.75f); // reverse operation of the perlin calculation
+                    float normalizeHeight = (noiseMap[x, y] + 1) / (maxPossibleHeight);
+                    noiseMap[x, y] = Mathf.Clamp(normalizeHeight, 0, int.MaxValue);
+                }
             }
         }
 
